055644538514ee458e3f5bb1561aa16eeb4c064b
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-10-03 01:09:41 -0300

    add get request
    added a get request that fetch a list of all products inserted

--- server.js
+++ server.js
@@ -18,8 +18,31 @@ app.post('/product', function(req, res) {
     product.title = req.body.title;
     product.price = req.body.price;
     // the likes is default 0 because we create a new product
+    product.save(function(err, savedProduct){
+        if (err) {
+            res.status(500).send({error:"Could not save product"}); 
+        } else {
+            res.status(200).send(savedProduct); // for status 200 we can ommit .status
+            // also, we can replace .send to .json
+        }
+    });
+});
+
+app.get('/product', function(req,res) {
+    
+    Product.find({}, function(err, products) {
+       if (err) {
+           res.status(500).send({error:"Could not fetch products"});
+       } else {
+           res.send(products);
+       }
+    });
 });
 
 app.listen(3000, function() {
     console.log("Swag Shop API running on port 3000...");
 });
+
+// mongodb://ademir:1ruthe@ds139715.mlab.com:39715/m-transactions
+
+// mongodb://localhost/swagshop
